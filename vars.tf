59d262d5b2d40a81c720e5261887f9d6f0fdf90d
lucasbrito3001 <lucas.brito3001@gmail.com>
2024-05-17 20:40:51 -0300

    ci: Add .env in docker run command

--- vars.tf
+++ vars.tf
@@ -10,6 +10,14 @@ variable "subnet_ip_range" {
   type = string
 }
 
+variable "sql_subnet_ip" {
+  type = string
+}
+
+variable "gce_static_ip" {
+  type = string
+}
+
 variable "services_machine_type" {
   type = string
 }
